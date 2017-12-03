859547c4893b25a4523b8ba12cd7eb5f1e272e90
Rachael Booth <rachaelmsb@gmail.com>
2017-12-03 23:54:47 +0000

    Day 3

---
+++ src/day3/part1.ts
@@ -0,0 +1,6 @@
+export function solve(): string {
+    // I actually solved this with just a calculator
+    // Input was a little less than 515^2, and calculated distance from side and number of rows down
+    // Answer here for completeness
+    return '438';
+}
---
+++ src/day3/part2.ts
@@ -0,0 +1,6 @@
+export function solve(): string {
+    // Also no code - conveniently I can avoid generating the spiral by looking it up
+    // at https://oeis.org/A141481
+    // Answer here for completeness
+    return '266330';
+}
