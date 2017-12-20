ce238b74b2b5f65b5ace7090ddc562046fbbb72d
Rachael Booth <rachael.booth@softwire.com>
2017-12-20 10:40:40 +0000

    Day 20

---
+++ src/day20/part2.ts
@@ -0,0 +1,81 @@
+import InputParser from '../utils/inputParser';
+import * as _ from 'lodash';
+
+const day = 20;
+const inputParser = new InputParser(day);
+
+export function solve(): string {
+    const input = inputParser.readInputLines();
+    let particles = _.map(input, (line, index) => parseLine(line, index));
+
+    let s = 0;
+    const probablyLongEnough = 10000;
+    while (s < probablyLongEnough) {
+        particles = _.map(particles, p => doStep(p));
+        particles = removeCollisions(particles);
+        s++;
+    }
+
+    return particles.length;
+}
+
+function removeCollisions(particles: particle[]): particle[] {
+    const grouped = _.groupBy(particles, p => p.position);
+    const noCollisions = _.pickBy(grouped, pos => pos.length === 1);
+    return _.flatten(_.values(noCollisions));
+}
+
+function doStep(particle: particle): particle {
+    return {
+        name: particle.name,
+        position: getNewPosition(particle),
+        velocity: getNewVelocity(particle),
+        accelleration: particle.accelleration
+    }
+}
+
+function getNewPosition(particle: particle): [number, number, number] {
+    return [
+        particle.position[0] + particle.velocity[0] + particle.accelleration[0],
+        particle.position[1] + particle.velocity[1] + particle.accelleration[1],
+        particle.position[2] + particle.velocity[2] + particle.accelleration[2]
+    ]
+}
+
+function getNewVelocity(particle: particle): [number, number, number] {
+    return [
+        particle.velocity[0] + particle.accelleration[0],
+        particle.velocity[1] + particle.accelleration[1],
+        particle.velocity[2] + particle.accelleration[2]
+    ]
+}
+
+function getDistanceFromZero(particle: particle): number {
+    return Math.abs(particle.position[0]) + Math.abs(particle.position[1]) + Math.abs(particle.position[2]);
+}
+
+function getAccelerationMod(particle: particle): number {
+    return Math.abs(particle.accelleration[0]) + Math.abs(particle.accelleration[1]) + Math.abs(particle.accelleration[2]);
+}
+
+function parseLine(line: string, index: number): particle {
+    const parts = line.split(', ');
+    return {
+        name: index,
+        position: parsePart(parts[0]),
+        velocity: parsePart(parts[1]),
+        accelleration: parsePart(parts[2])
+    }
+}
+
+function parsePart(part: string): [number, number, number] {
+    const elements = part.substring(3, part.length - 1).split(',');
+    return [_.parseInt(elements[0]), _.parseInt(elements[1]), _.parseInt(elements[2])];
+}
+
+interface particle {
+    name: number,
+    position: [number, number, number],
+    velocity: [number, number, number],
+    accelleration: [number, number, number]
+}
